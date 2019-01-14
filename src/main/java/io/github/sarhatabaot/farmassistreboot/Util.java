f06803ed13deb5726db2be754a88f9872d63fd94
sarhatabaot <sarhatabaot1@gmail.com>
2019-01-14 21:03:59 +0200

    Changed to crop form

--- src/main/java/io/github/sarhatabaot/farmassistreboot/Util.java
+++ src/main/java/io/github/sarhatabaot/farmassistreboot/Util.java
@@ -15,16 +15,27 @@ public class Util {
 
     public static boolean inventoryContains(Player player, Material material){
         switch (material){
+            case COCOA:
+                FarmAssistReboot.debug("COCOA_BEANS:"+player.getInventory().contains(Material.COCOA_BEANS));
+                return player.getInventory().contains(Material.COCOA_BEANS);
+            case POTATOES:
+                FarmAssistReboot.debug("POTATO:"+player.getInventory().contains(Material.POTATO));
+                return player.getInventory().contains(Material.POTATO);
+            case CARROTS:
+                FarmAssistReboot.debug("CARROT"+player.getInventory().contains(Material.CARROT));
+                return player.getInventory().contains(Material.CARROT);
             case WHEAT:
                 FarmAssistReboot.debug("WHEAT_SEEDS:"+player.getInventory().contains(Material.WHEAT_SEEDS));
                 return player.getInventory().contains(Material.WHEAT_SEEDS);
-            case BEETROOT:
+            case BEETROOTS:
                 FarmAssistReboot.debug("BEETROOT_SEEDS:"+player.getInventory().contains(Material.BEETROOT_SEEDS));
                 return player.getInventory().contains(Material.BEETROOT_SEEDS);
-            case PUMPKIN:
+            case PUMPKIN_STEM:
+            case ATTACHED_PUMPKIN_STEM:
                 FarmAssistReboot.debug("PUMPKIN_SEEDS:"+player.getInventory().contains(Material.PUMPKIN_SEEDS));
                 return player.getInventory().contains(Material.PUMPKIN_SEEDS);
-            case MELON:
+            case MELON_STEM:
+            case ATTACHED_MELON_STEM:
                 FarmAssistReboot.debug("MELON_SEEDS:"+player.getInventory().contains(Material.MELON_SEEDS));
                 return player.getInventory().contains(Material.MELON_SEEDS);
             default: {
