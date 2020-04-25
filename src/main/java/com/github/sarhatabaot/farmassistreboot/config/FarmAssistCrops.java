45d865350064598e92a44d4e772de5b93254a4c5
Omer Oreg <sarhatabaot1@gmail.com>
2020-04-25 09:48:02 +0300

    Changed list to immutablelist

--- src/main/java/com/github/sarhatabaot/farmassistreboot/config/FarmAssistCrops.java
+++ src/main/java/com/github/sarhatabaot/farmassistreboot/config/FarmAssistCrops.java
@@ -1,5 +1,6 @@
 package com.github.sarhatabaot.farmassistreboot.config;
 
+import com.google.common.collect.ImmutableList;
 import org.bukkit.Material;
 
 import java.util.ArrayList;
@@ -9,26 +10,23 @@ import java.util.List;
  * @author sarhatabaot
  */
 public class FarmAssistCrops {
-    private static List<Material> cropList = createCropList();
+    private static ImmutableList<Material> cropList = ImmutableList.of(
+            Material.WHEAT,
+            Material.SUGAR_CANE,
+            Material.NETHER_WART,
+            Material.COCOA,
+            Material.CARROTS,
+            Material.POTATOES,
+            Material.ATTACHED_MELON_STEM,
+            Material.ATTACHED_PUMPKIN_STEM,
+            Material.PUMPKIN_STEM,
+            Material.MELON_STEM,
+            Material.BEETROOTS,
+            Material.CACTUS
+    );
 
-    private static List<Material> createCropList(){
-        ArrayList<Material> cropList = new ArrayList<>();
-        cropList.add(Material.WHEAT);
-        cropList.add(Material.SUGAR_CANE);
-        cropList.add(Material.NETHER_WART);
-        cropList.add(Material.COCOA);
-        cropList.add(Material.CARROTS);
-        cropList.add(Material.POTATOES);
-        cropList.add(Material.ATTACHED_MELON_STEM);
-        cropList.add(Material.ATTACHED_PUMPKIN_STEM);
-        cropList.add(Material.PUMPKIN_STEM);
-        cropList.add(Material.MELON_STEM);
-        cropList.add(Material.BEETROOTS);
-        cropList.add(Material.CACTUS);
-        return cropList;
-    }
 
-    public static List<Material> getCropList() {
+    public static ImmutableList<Material> getCropList() {
         return cropList;
     }
 }
