ccc84f7b82678b29474b42a225d688a6b7d0a0fd
Omer Oreg <sarhatabaot1@gmail.com>
2021-02-08 22:04:31 +0200

    Removed debug messages

--- src/main/java/com/github/sarhatabaot/farmassistreboot/Util.java
+++ src/main/java/com/github/sarhatabaot/farmassistreboot/Util.java
@@ -4,39 +4,33 @@ import com.github.sarhatabaot.farmassistreboot.config.FarmAssistConfig;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 
 /**
  * @author sarhatabaot
  */
 public class Util {
-    public static boolean inventoryContains(Player player, Material material){
+    public static boolean inventoryContains(PlayerInventory playerInventory, Material material){
         switch (material){
             case COCOA:
-                FarmAssistReboot.debug("COCOA_BEANS:"+player.getInventory().contains(Material.COCOA_BEANS));
-                return player.getInventory().contains(Material.COCOA_BEANS);
+                return playerInventory.contains(Material.COCOA_BEANS);
             case POTATOES:
-                FarmAssistReboot.debug("POTATO:"+player.getInventory().contains(Material.POTATO));
-                return player.getInventory().contains(Material.POTATO);
+                return playerInventory.contains(Material.POTATO);
             case CARROTS:
-                FarmAssistReboot.debug("CARROT"+player.getInventory().contains(Material.CARROT));
-                return player.getInventory().contains(Material.CARROT);
+                return playerInventory.contains(Material.CARROT);
             case WHEAT:
-                FarmAssistReboot.debug("WHEAT_SEEDS:"+player.getInventory().contains(Material.WHEAT_SEEDS));
-                return player.getInventory().contains(Material.WHEAT_SEEDS);
+                return playerInventory.contains(Material.WHEAT_SEEDS);
             case BEETROOTS:
-                FarmAssistReboot.debug("BEETROOT_SEEDS:"+player.getInventory().contains(Material.BEETROOT_SEEDS));
-                return player.getInventory().contains(Material.BEETROOT_SEEDS);
+                return playerInventory.contains(Material.BEETROOT_SEEDS);
             case PUMPKIN_STEM:
             case ATTACHED_PUMPKIN_STEM:
-                FarmAssistReboot.debug("PUMPKIN_SEEDS:"+player.getInventory().contains(Material.PUMPKIN_SEEDS));
-                return player.getInventory().contains(Material.PUMPKIN_SEEDS);
+                return playerInventory.contains(Material.PUMPKIN_SEEDS);
             case MELON_STEM:
             case ATTACHED_MELON_STEM:
-                FarmAssistReboot.debug("MELON_SEEDS:"+player.getInventory().contains(Material.MELON_SEEDS));
-                return player.getInventory().contains(Material.MELON_SEEDS);
+                return playerInventory.contains(Material.MELON_SEEDS);
             default: {
-                FarmAssistReboot.debug(material.name()+":"+player.getInventory().contains(material));
-                return player.getInventory().contains(material);
+                FarmAssistReboot.debug(material.name()+":"+playerInventory.contains(material));
+                return playerInventory.contains(material);
             }
         }
     }
