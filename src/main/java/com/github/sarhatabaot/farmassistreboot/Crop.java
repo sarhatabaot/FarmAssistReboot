52d4b63fb5627445a6a47017a2b6654b9799ac13
Omer Oreg <sarhatabaot1@gmail.com>
2021-02-09 21:11:20 +0200

    Changed to Material array to accomadate sugar cane

--- src/main/java/com/github/sarhatabaot/farmassistreboot/Crop.java
+++ src/main/java/com/github/sarhatabaot/farmassistreboot/Crop.java
@@ -4,29 +4,29 @@ package com.github.sarhatabaot.farmassistreboot;
 import org.bukkit.Material;
 
 public enum Crop {
-	WHEAT(Material.FARMLAND, Material.WHEAT, Material.WHEAT_SEEDS),
-	SUGAR_CANE(Material.GRASS, Material.SUGAR_CANE, Material.SUGAR_CANE),
-	NETHER_WART(Material.SOUL_SAND, Material.NETHER_WART, Material.NETHER_WART),
-	COCOA(Material.JUNGLE_LOG, Material.COCOA, Material.COCOA),
-	CARROTS(Material.FARMLAND, Material.CARROTS, Material.CARROT),
-	POTATOES(Material.FARMLAND, Material.POTATOES, Material.POTATO),
-	BEETROOT(Material.FARMLAND,Material.BEETROOTS,Material.BEETROOT_SEEDS),
-	CACTUS(Material.SAND,Material.CACTUS,Material.CACTUS)
+	WHEAT(new Material[]{Material.FARMLAND}, Material.WHEAT, Material.WHEAT_SEEDS),
+	SUGAR_CANE(new Material[]{Material.GRASS,Material.SAND,Material.DIRT}, Material.SUGAR_CANE, Material.SUGAR_CANE),
+	NETHER_WART(new Material[]{Material.SOUL_SAND}, Material.NETHER_WART, Material.NETHER_WART),
+	COCOA(new Material[]{Material.JUNGLE_LOG}, Material.COCOA, Material.COCOA),
+	CARROTS(new Material[]{Material.FARMLAND}, Material.CARROTS, Material.CARROT),
+	POTATOES(new Material[]{Material.FARMLAND}, Material.POTATOES, Material.POTATO),
+	BEETROOT(new Material[]{Material.FARMLAND},Material.BEETROOTS,Material.BEETROOT_SEEDS),
+	CACTUS(new Material[]{Material.SAND},Material.CACTUS,Material.CACTUS)
 	;
 
-	private final Material plantedOn;
+	private final Material[] plantedOn;
 	private final Material planted;
 	private final Material seed;
 
 
-	Crop(final Material plantedOn, final Material planted, final Material seed) {
+	Crop(final Material[] plantedOn, final Material planted, final Material seed) {
 		this.plantedOn = plantedOn;
 		this.planted = planted;
 		this.seed = seed;
 	}
 
 
-	public Material getPlantedOn() {
+	public Material[] getPlantedOn() {
 		return plantedOn;
 	}
 
