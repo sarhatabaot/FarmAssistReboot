93e8722f5e3751350e9f6e47113336753f670b99
Omer Oreg <sarhatabaot1@gmail.com>
2022-10-16 17:12:15 +0300

    Fixes an issue where cocoa wouldn't replant

--- src/main/java/com/github/sarhatabaot/farmassistreboot/Crop.java
+++ src/main/java/com/github/sarhatabaot/farmassistreboot/Crop.java
@@ -7,7 +7,7 @@ public enum Crop {
 	WHEAT(new Material[]{Material.FARMLAND}, Material.WHEAT, Material.WHEAT_SEEDS),
 	SUGAR_CANE(new Material[]{Material.GRASS_BLOCK,Material.SAND,Material.DIRT}, Material.SUGAR_CANE, Material.SUGAR_CANE),
 	NETHER_WART(new Material[]{Material.SOUL_SAND}, Material.NETHER_WART, Material.NETHER_WART),
-	COCOA(new Material[]{Material.JUNGLE_LOG, Material.STRIPPED_JUNGLE_LOG, Material.STRIPPED_JUNGLE_WOOD, Material.JUNGLE_WOOD}, Material.COCOA, Material.COCOA),
+	COCOA(new Material[]{Material.JUNGLE_LOG, Material.STRIPPED_JUNGLE_LOG, Material.STRIPPED_JUNGLE_WOOD, Material.JUNGLE_WOOD}, Material.COCOA, Material.COCOA_BEANS),
 	CARROTS(new Material[]{Material.FARMLAND}, Material.CARROTS, Material.CARROT),
 	POTATOES(new Material[]{Material.FARMLAND}, Material.POTATOES, Material.POTATO),
 	BEETROOTS(new Material[]{Material.FARMLAND},Material.BEETROOTS,Material.BEETROOT_SEEDS),
