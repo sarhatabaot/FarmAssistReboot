240aa7a18e1cf1b2c74e75fb97ee9644d702018b
Omer Oreg <omeroreg@gmail.com>
2024-08-23 11:59:54 +0300

    Minor formatting fixes

--- src/main/java/com/github/sarhatabaot/farmassistreboot/listeners/BlockBreakListener.java
+++ src/main/java/com/github/sarhatabaot/farmassistreboot/listeners/BlockBreakListener.java
@@ -20,77 +20,79 @@ import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
 public class BlockBreakListener implements Listener {
-	private final FarmAssistReboot plugin;
+    private final FarmAssistReboot plugin;
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onBlockBreak(BlockBreakEvent event) {
-		if (!plugin.isGlobalEnabled())
-			return;
-
-		if (this.plugin.getDisabledPlayerList().contains(event.getPlayer().getUniqueId()))
-			return;
-
-
-		if (!Crop.getCropList().contains(event.getBlock().getType())) {
-			debug(Debug.OnBlockBreak.CROP_LIST_NO_MATERIAL, event.getBlock().getType().name());
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakEvent event) {
+		if (!plugin.isGlobalEnabled()) {
 			return;
 		}
 
-		if (FarmAssistConfig.USE_PERMISSIONS && !hasMaterialPermission(event)) {
-			final String permission = Permissions.BASE_PERMISSION + event.getBlock().getType().name();
-			debug(Debug.OnBlockBreak.PLAYER_NO_PERMISSION,event.getPlayer().getDisplayName(),permission);
-			return;
-		}
-
-		if (Util.isWorldEnabled(event.getPlayer().getWorld())) {
-			applyReplant(event);
-		}
-	}
-
-	private boolean hasMaterialPermission(@NotNull BlockBreakEvent event) {
-		return event.getPlayer().hasPermission(Permissions.BASE_PERMISSION + event.getBlock().getType().name());
-	}
-
-	private void applyReplant(@NotNull BlockBreakEvent event) {
-		Material material = event.getBlock().getType();
-		debug(Debug.OnBlockBreak.BLOCK_BROKEN,material.name());
-		if (!Crop.getCropList().contains(material)) {
-			debug(Debug.OnBlockBreak.CROP_LIST_NO_MATERIAL,material.name());
-			return;
-		}
-
-		debug(Debug.OnBlockBreak.CROP_LIST_CONTAINS,material.name());
-		if (!FarmAssistConfig.getEnabled(material)) {
-			debug(Debug.OnBlockBreak.MATERIAL_DISABLED,material.name());
-			return;
-		}
-
-		int slot = Util.inventoryContainsSeeds(event.getPlayer().getInventory(), material);
-		if (!Util.checkNoSeeds(event.getPlayer()) && slot == -1) {
-			debug(Debug.OnBlockBreak.NO_SEEDS,event.getPlayer().getName());
+		if (this.plugin.getDisabledPlayerList().contains(event.getPlayer().getUniqueId())) {
 			return;
 		}
 
 
-		if (material == Material.SUGAR_CANE || material == Material.CACTUS) {
-			Util.replant(event.getPlayer(), event.getBlock(), material);
-			return;
-		}
+        if (!Crop.getCropList().contains(event.getBlock().getType())) {
+            debug(Debug.OnBlockBreak.CROP_LIST_NO_MATERIAL, event.getBlock().getType().name());
+            return;
+        }
 
-		if (!FarmAssistConfig.getRipe(material) || isRipe(event.getBlock())) {
-			debug(String.format("isRipeConfig %s: ", material) + FarmAssistConfig.getRipe(material));
-			debug(String.format("isRipe %s: ", material) + isRipe(event.getBlock()));
-			Util.replant(event.getPlayer(), event.getBlock(), slot);
-		}
-	}
-	
+        if (FarmAssistConfig.USE_PERMISSIONS && !hasMaterialPermission(event)) {
+            final String permission = Permissions.BASE_PERMISSION + event.getBlock().getType().name();
+            debug(Debug.OnBlockBreak.PLAYER_NO_PERMISSION, event.getPlayer().getDisplayName(), permission);
+            return;
+        }
 
-	private boolean isRipe(@NotNull Block block) {
-		Ageable age = (Ageable) block.getBlockData();
-		return (age.getAge() == age.getMaximumAge());
-	}
+        if (Util.isWorldEnabled(event.getPlayer().getWorld())) {
+            applyReplant(event);
+        }
+    }
 
-	private void debug(final String message, Object... args){
-		plugin.debug(BlockBreakListener.class,String.format(message,args));
-	}
+    private boolean hasMaterialPermission(@NotNull BlockBreakEvent event) {
+        return event.getPlayer().hasPermission(Permissions.BASE_PERMISSION + event.getBlock().getType().name());
+    }
+
+    private void applyReplant(@NotNull BlockBreakEvent event) {
+        Material material = event.getBlock().getType();
+        debug(Debug.OnBlockBreak.BLOCK_BROKEN, material.name());
+        if (!Crop.getCropList().contains(material)) {
+            debug(Debug.OnBlockBreak.CROP_LIST_NO_MATERIAL, material.name());
+            return;
+        }
+
+        debug(Debug.OnBlockBreak.CROP_LIST_CONTAINS, material.name());
+        if (!FarmAssistConfig.getEnabled(material)) {
+            debug(Debug.OnBlockBreak.MATERIAL_DISABLED, material.name());
+            return;
+        }
+
+        int slot = Util.inventoryContainsSeeds(event.getPlayer().getInventory(), material);
+        if (!Util.checkNoSeeds(event.getPlayer()) && slot == -1) {
+            debug(Debug.OnBlockBreak.NO_SEEDS, event.getPlayer().getName());
+            return;
+        }
+
+
+        if (material == Material.SUGAR_CANE || material == Material.CACTUS) {
+            Util.replant(event.getPlayer(), event.getBlock(), material);
+            return;
+        }
+
+        if (!FarmAssistConfig.getRipe(material) || isRipe(event.getBlock())) {
+            debug(String.format("isRipeConfig %s: ", material) + FarmAssistConfig.getRipe(material));
+            debug(String.format("isRipe %s: ", material) + isRipe(event.getBlock()));
+            Util.replant(event.getPlayer(), event.getBlock(), slot);
+        }
+    }
+
+
+    private boolean isRipe(@NotNull Block block) {
+        Ageable age = (Ageable) block.getBlockData();
+        return (age.getAge() == age.getMaximumAge());
+    }
+
+    private void debug(final String message, Object... args) {
+        plugin.debug(BlockBreakListener.class, String.format(message, args));
+    }
 }
